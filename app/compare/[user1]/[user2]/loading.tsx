a091252be484904999b3f6f3e8acd5a1a7d02245
29cmb <devcmb@outlook.com>
2026-04-11 10:34:42 -0400

    feat: :sparkles: Add a loading screen for the compare view

---
+++ app/compare/[user1]/[user2]/loading.tsx
@@ -0,0 +1,5 @@
+export default function LoadingPage() {
+    return <div className="flex m-auto justify-center items-center">
+        <p className="font-bold text-3xl">Loading...</p>
+    </div>
+}
